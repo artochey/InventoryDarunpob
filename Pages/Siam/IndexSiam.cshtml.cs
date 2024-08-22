68872bef30c0dd1565d25adff232531a2d800902
DARUNPOB NAJAROON <darunpob.naja@bu.ac.th>
2024-08-22 22:22:37 +0700

    Finish

---
+++ Pages/Siam/IndexSiam.cshtml.cs
@@ -0,0 +1,58 @@
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace InventoryDarunpob.Pages.Siam
+{
+	public class IndexSiamModel : PageModel
+	{
+		public List<StockInfo> listStock = new List<StockInfo>();
+
+		public void OnGet()
+		{
+			try
+			{
+				String connectionString = "Server=tcp:darunpob.database.windows.net,1433;Initial Catalog=Darunpob;Persist Security Info=False;User ID=darunpob;Password=Artart2547;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string sql = "SELECT * FROM stocks WHERE storeid=2 ";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								StockInfo stockinfo = new StockInfo();
+								stockinfo.itemid = "" + reader.GetInt32(0);
+								stockinfo.item = reader.GetString(1);
+								stockinfo.storeid = reader.GetString(2);
+								stockinfo.supplier = reader.GetString(3);
+								stockinfo.amount = reader.GetString(4);
+								stockinfo.create_at = reader.GetDateTime(5).ToString();
+
+								listStock.Add(stockinfo);
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine("Exception: " + ex.ToString());
+			}
+		}
+	}
+
+
+	public class StockInfo
+	{
+		public String itemid;
+		public String item;
+		public String storeid;
+		public String supplier;
+		public String amount;
+		public String create_at;
+	}
+}
